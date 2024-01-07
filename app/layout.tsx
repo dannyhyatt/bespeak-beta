959f91966104789334e5bba29ce46f69c7a65500
danny hyatt <daf281@aol.com>
2024-01-06 20:10:49 -0500

    pwa ???

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,37 @@
 import { GeistSans } from 'geist/font/sans'
 import './globals.css'
 import Head from 'next/head'
+import { Viewport } from 'next';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : 'http://localhost:3000'
 
+
+const APP_NAME = "bespeak";
+const APP_DEFAULT_TITLE = "bespeak";
+const APP_TITLE_TEMPLATE = "%s | bespeak";
+const APP_DESCRIPTION = "a social network for writers";
+
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'bespeak',
-  description: 'a social network for writers',
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
   manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
 }
+
+export const viewport: Viewport = {
+  themeColor: "#EEEEEE",
+};
 
 export default function RootLayout({
   children,
