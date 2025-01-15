07a0c05ebc2cfe7d59c29d7af5827b9453868146
danny hyatt <daf281@aol.com>
2025-01-14 20:06:25 -0500

    random commit to refresh vercel

--- components/ExpandableParagraph.tsx
+++ components/ExpandableParagraph.tsx
@@ -3,20 +3,20 @@
 import { IconArrowUp, IconChevronUp } from "@tabler/icons-react"
 import { useState } from "react"
 
-export default function ExpandableParagraph({ children, className, ...props } : { children: React.ReactNode, className?: string }) {
-  
-    const [expanded, setExpanded] = useState<boolean>(false)
-  
-    return (
-      <p 
-        className={`${className} ${expanded ? '' : 'line-clamp-3'}`}
-        {...props}
-        onClick={() => { if(!expanded) setExpanded(true) }}
-      >
-        {children}
-        {expanded && 
-          <IconChevronUp className={`inline align-top bg-gray-200 dark:bg-gray-800 m-1 mt-0.5 rounded-md cursor-pointer`} onClick={e => setExpanded(false)} />
-        }
-      </p>
-    )
-  }
+export default function ExpandableParagraph({ children, className, ...props }: { children: React.ReactNode, className?: string }) {
+
+  const [expanded, setExpanded] = useState<boolean>(false)
+
+  return (
+    <p
+      className={`${className} ${expanded ? '' : 'line-clamp-3'}`}
+      {...props}
+      onClick={() => { if (!expanded) setExpanded(true) }}
+    >
+      {children}
+      {expanded &&
+        <IconChevronUp className={`inline align-top bg-gray-200 dark:bg-gray-800 m-1 mt-0.5 rounded-md cursor-pointer`} onClick={e => setExpanded(false)} />
+      }
+    </p>
+  )
+}
