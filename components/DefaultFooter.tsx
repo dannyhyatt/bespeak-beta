bd347eb1aab8a5878772c94c1f806b2cd06719b4
danny hyatt <daf281@aol.com>
2025-01-14 19:12:48 -0500

    new auth credentials

--- components/DefaultFooter.tsx
+++ components/DefaultFooter.tsx
@@ -1,12 +1,12 @@
 
 const DefaultFooter = () => {
   return (
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          &copy; 2024 Bespeak
-        </p>
-      </footer>
-    )
-  }
+    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      <p>
+        &copy; 2025 Bespeak
+      </p>
+    </footer>
+  )
+}
 
 export default DefaultFooter
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,21 +7,22 @@ import Link from "next/link";
 const { convert } = require('html-to-text'); // Import the library
 
 
-export function PostCard({ post, displayAuthor } : { post: PostWithRevision, displayAuthor?: boolean }) {
+export function PostCard({ post, displayAuthor }: { post: PostWithRevision, displayAuthor?: boolean }) {
 
-  if(!post) return
+  if (!post) return
 
   return (
-    <Link href={`/post/${post.id}`} className="flex flex-col mb-4 rounded-md shadow-lg dark:border-grey-50 border-2 p-4 sm:mx-[-1rem]">
+    <Link href={`/post/${post.id}`} className="flex flex-col mb-4 rounded-md shadow-lg hover:shadow-xl transition-all dark:border-grey-50 border-2 p-4 sm:mx-[-1rem]">
       {post.cover_image && <img src={post.cover_image} className="mb-2" />}
       <div className="text-xl font-bold">{post.title ?? <><IconEyeOff className="inline text-red-500" /> Your Private Post</>}</div>
       <div className="line-clamp-3 my-2">{post.text_only_content}</div>
       <div className="text-gray-700 dark:text-gray-300 text-sm flex justify-between">
         <span>
-          { displayAuthor && 
+          {displayAuthor &&
             <>by<img
               className="inline lg:h-8 md:h-6 h-4 mx-1 md:mx-2 aspect-square rounded-md align-middle"
-              src={getAvatarUrl(createClient(), { ...post,
+              src={getAvatarUrl(createClient(), {
+                ...post,
                 id: post.author_id,
                 full_name: post.author_name,
                 username: post.username,
@@ -29,7 +30,7 @@ export function PostCard({ post, displayAuthor } : { post: PostWithRevision, dis
               })} />
               {post.author_name} on {' '}
             </>
-          } 
+          }
           {format(parseISO(post.created_at), 'LLLL d, yyyy')}
         </span>
         <span className="[&>*]:h-4 [&>svg]:mr-1 [&>:not(svg)]:ml-1 flex items-center">
